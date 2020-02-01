9bb5eb65ab6041dc3331ae9d6e399db56e670a65
karthik koppaka <officialkarthikk@gmail.com>
2020-02-01 14:36:25 +0530

    created manifest.json for pwas

--- src/node.js
+++ src/node.js
@@ -1,9 +1,5 @@
 const app = require("express")();
 app.listen(8080);
-app.get("/*", (req, res) => {
-  console.log(req.url);
-  res.sendFile(__dirname + "/www" + req.url);
-});
 app.get("/provider/*", (req, res) => {
   res.sendFile(__dirname + "/" + req.url);
 });
@@ -12,4 +8,8 @@ app.get("/verifier/*", (req, res) => {
 });
 app.get("/api/*", (req, res) => {
   res.send("not Ready Yet");
+});
+app.get("/*", (req, res) => {
+  console.log(req.url);
+  res.sendFile(__dirname + "/www" + req.url);
 });
